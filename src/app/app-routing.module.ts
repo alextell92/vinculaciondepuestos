2ca3731ca5d9c7d3bf4e54e20b7050a58cfb06a5
MARIA CONCEPCION GONZALEZ GONZALEZ <DGTITMARIAGO@guanajuato.gob.mx>
2025-04-29 18:42:44 -0600

    Cambios en contenido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { HomeComponent } from './home/home.component';
 import { ValuacionPuestosFormComponent} from './components/valuacion-puestos-form/valuacion-puestos-form.component'
 import { VerinformacionComponent } from './verinformacion/verinformacion.component';
 import { EscalasPuntuacionFormComponent } from './components/escalas-puntuacion-form/escalas-puntuacion-form.component';
+import { ComparacionSalarialFormComponent } from './components/comparacion-salarial-form/comparacion-salarial-form.component';
+import { PlanComunicacionFormComponent } from './components/plan-comunicacion-form/plan-comunicacion-form.component';
 
 const routes: Routes = [
   { path: "Home", component: HomeComponent },
   { path : "valuacionPuestos", component:ValuacionPuestosFormComponent},
   {path: "verInformacion",component:VerinformacionComponent},
   {path: "escalasPuntuacion",component:EscalasPuntuacionFormComponent},
+  {path: "comparacionSalarial",component:ComparacionSalarialFormComponent},
+  {path: "planComunicacion",component:PlanComunicacionFormComponent},
+
 
   //Esta ruta, siempre debe ir al final, de lo contrario no permite navegar a las rutas que coloquemos despues
   { path: "**", pathMatch: "full", redirectTo: "/Home" },
